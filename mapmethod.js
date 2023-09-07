d68a1184420c2fbd66c3429f6c2b36e7244247d1
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-09-07 18:31:35 +0545

    javascript_method

--- mapmethod.js
+++ mapmethod.js
@@ -8,3 +8,16 @@ let c=arr2.map((value,index)=>{
 })
 
 console.log(c);
+
+//reduce method
+
+let arr1=[10,20,30]
+
+let b=arr1.reduce((num1,num2)=>
+ {
+  
+  return num1-num2
+})
+
+console.log(b);
+
